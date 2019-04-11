715c4c60ecbf3e09c87ed984d3a6788ecf1a12db
Hardik082021 <49453368+Hardik082021@users.noreply.github.com>
2019-04-11 21:45:14 +0530

    Add files via upload

--- bankers.c
+++ bankers.c
@@ -1,8 +1,89 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-
-
+const int R=5;
+const int P=5;
+// Function to find the system is in safe state or not 
+bool Safe(int processes[], int avail[], int maxm[][R], 
+            int allot[][R]) 
+{ 
+    int need[P][R]; 
+  
+    // Function to calculate need matrix 
+    calculateNeed(need, maxm, allot); 
+  
+    // Mark all processes as infinish 
+    bool finish[P] = {0}; 
+  
+    // To store safe sequence 
+    int safeSeq[P]; 
+  
+    // Make a copy of available resources 
+    int work[R]; 
+    for (int i = 0; i < R ; i++) 
+        work[i] = avail[i]; 
+  
+    // While all processes are not finished 
+    // or system is not in safe state. 
+    int count = 0; 
+    while (count < P) 
+    { 
+        // Find a process which is not finish and 
+        // whose needs can be satisfied with current 
+        // work[] resources. 
+        bool found = false; 
+        for (int p = 0; p < P; p++) 
+        { 
+            // First check if a process is finished, 
+            // if no, go for next condition 
+            if (finish[p] == 0) 
+            { 
+                // Check if for all resources of 
+                // current P need is less 
+                // than work 
+                int j; 
+                for (j = 0; j < R; j++) 
+                    if (need[p][j] > work[j]) 
+                        break; 
+  
+                // If all needs of p were satisfied. 
+                if (j == R) 
+                { 
+                    // Add the allocated resources of 
+                    // current P to the available/work 
+                    // resources i.e.free the resources 
+                    for (int k = 0 ; k < R ; k++) 
+                        work[k] += allot[p][k]; 
+  
+                    // Add this process to safe sequence. 
+                    safeSeq[count++] = p; 
+  
+                    // Mark this p as finished 
+                    finish[p] = 1; 
+  
+                    found = true; 
+                } 
+            } 
+        } 
+  
+        // If we could not find a next process in safe 
+        // sequence. 
+        if (found == false) 
+        { 
+            cout << "System is not in safe state"; 
+            return false; 
+        } 
+    } 
+  
+    // If system is in safe state then 
+    // safe sequence will be as below 
+    cout << "System is in safe state.\nSafe"
+         " sequence is: "; 
+    for (int i = 0; i < P ; i++) 
+        cout << safeSeq[i] << " "; 
+  
+    return true; 
+} 
 int main()
 {
 	int processes[5] = {0, 1, 2, 3, 4};
